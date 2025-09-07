8ec9fbafeef017a154d2a4e07fec7892b7a38a7a
Vaibhav Sharma <24bee103@nith.ac.in>
2025-09-07 15:10:07 +0530

    Delete part3 directory

--- part3/conditions.js
+++
@@ -1,7 +0,0 @@
-let num1 = 11
-let num2 = 22
-if (num1 > num2) {
-    console.log(true);
-} else {
-    console.log(false);
-}
--- part3/function.js
+++
@@ -1,5 +0,0 @@
-function makeTea(typeofTea){//Function Declaratio
-    return `Making${typeofTea}`;//Return Statement
-}
-let teaOrder=makeTea("lemon tea")
-console.log(teaOrder)

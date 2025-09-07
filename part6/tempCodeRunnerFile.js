e635d0c28de823961908ddb392fef92d92e75165
Vaibhav Sharma <24bee103@nith.ac.in>
2025-09-07 15:10:32 +0530

    Delete part6 directory

--- part6/tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-Array.prototype.hitesh=function(){
-//     return`Custom method ${this}`
-// }
-
-// let myArray=[1,2,3]
-// // console.log(myArray.hitesh())
-// let myNewArray=[1,2,3,4,5,6]
-// // // console.log(myArray.hites
